991c7f99d88bbdc5e278ffaeaf81d453d6ca6f7e
alfiankan <alfiankan19@gmail.com>
2022-10-22 14:24:14 +0700

    chore(initial): init

---
+++ dump-env-app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"os/exec"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+
+		cmd := exec.Command("env")
+		envs, err := cmd.Output()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		return c.String(http.StatusOK, string(envs))
+	})
+	e.GET("/version", func(c echo.Context) error {
+
+		return c.String(http.StatusOK, "v1.0.0")
+	})
+	e.Logger.Fatal(e.Start(":8000"))
+}
