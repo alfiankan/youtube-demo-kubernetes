82143871e593c57f5abbe3facc8f486542ab8139
alfiandra <76727718+alfiandra@users.noreply.github.com>
2022-12-05 06:16:09 +0700

    Update index.js

--- majelist_service/index.js
+++ majelist_service/index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/health', (req, res) => {
-  res.status(200).send("v34.0.222909")
+  res.status(200).send("v2.0.0")
 })
 
 app.get('/quotes', (req, res) => {
